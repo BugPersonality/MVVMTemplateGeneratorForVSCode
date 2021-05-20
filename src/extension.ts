44e189ccd52fb94f5f3df1e8455fb0c3cdfe4a9d
BugPersonality <dubov.danil.01@mail.ru>
2021-05-20 13:09:37 +0300

    finish

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,38 @@
 import * as vscode from 'vscode';
 import { Model } from "./model";
-
+import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	const model = new Model();
 
 	let disposable = vscode.commands.registerCommand('mvvmgenerator.myExtension', () => {
-		
+
+		let pathToCurrentDir = vscode.workspace.rootPath?.toString()
+
+		if (pathToCurrentDir == null){
+			vscode.window.showErrorMessage("You should open project before use extension !!!");
+			// console.error("You should open project before use extension !!!");
+			return;
+		}
+
 		vscode.window.showInputBox({
 			placeHolder: 'Enter module name: '
 		}).then(function(data) {
 			let moduleName = data;
-			
-			let pathToCurrentDir = vscode.workspace.rootPath?.toString()
-			
-			if (pathToCurrentDir == null && moduleName == null){
-				console.error("You should open project before use extension !!!");
+
+			let pathToDir = pathToCurrentDir + "/" + moduleName;
+			console.log(pathToDir)
+
+			if (fs.existsSync(pathToDir)) {
+				vscode.window.showErrorMessage("Folder already exist !! Pls enter another module name");
+				// console.error("Folder already exist !! Pls enter another module name");
+				return;
+			}
+
+			if (moduleName == null){
+				vscode.window.showErrorMessage("Enter module name befor create module !!!");
+				//console.error("Enter module name befor create module !!!");
 				return;
 			}else {
 				model.createMVVMModule(pathToCurrentDir!, moduleName!);
--- src/model.ts
+++ src/model.ts
@@ -1,36 +1,54 @@
 import * as fs from 'fs';
+import * as vscode from 'vscode';
+import axios from 'axios';
+import { html } from 'cheerio';
+
+"use strict";
 
 export class Model {
+
+    text: string = "";
+
     createMVVMModule(pathToCurrentDir: String, name: String) {
         let pathToDir = pathToCurrentDir + "/" + name;
         this.createFolder(pathToDir);
 
         let filesNames = [name + "View.swift", name + "ViewModel.swift", name + ".swift", name + "UI.swift"];
 
+        let urls = [
+            'https://danil.s-ul.eu/ihkZzUn2', // ViewModel
+            'https://danil.s-ul.eu/IZdgTNrP', // View
+            'https://danil.s-ul.eu/9LYlupnJ', // VFile
+            'https://danil.s-ul.eu/pAw8hEXA'  // UI
+        ]
+
         filesNames.forEach((fileName) => {
             this.createFile(pathToDir, fileName);
-        });
+        }); 
 
         this.setTextForSwiftFile(
-            pathToDir + "/" + name + "View.swift",
-            '/Users/danildubov/mvvmgenerator/src/View.txt',
-            name
-            );
-        this.setTextForSwiftFile(
             pathToDir + "/" + name + "ViewModel.swift", 
-            '/Users/danildubov/mvvmgenerator/src/ViewModel.txt',
+            urls[0],
             name
             );
+        
+        this.setTextForSwiftFile(
+            pathToDir + "/" + name + "View.swift",
+            urls[1],
+            name
+            );
+        
         this.setTextForSwiftFile(
             pathToDir + "/" + name + ".swift", 
-            '/Users/danildubov/mvvmgenerator/src/VFile.txt',
+            urls[2],
             name
             );
+        
         this.setTextForSwiftFile(
             pathToDir + "/" + name + "UI.swift",
-            '/Users/danildubov/mvvmgenerator/src/UI.txt',
+            urls[3],
             name
-            );    
+            );  
     }
 
     private createFolder(path: String) {
@@ -42,14 +60,22 @@ export class Model {
         fs.createWriteStream(pathToDir + "/" + fileName).close();
     }
 
-    private setTextForSwiftFile(pathToWriteFile: String, pathToReadFile: String, moduleName: String) {
-        const data = fs.readFileSync(pathToReadFile.toString(), {
-            encoding:'utf8', flag:'r'
-        });
-        let newData = data.replace(/%_/gi, moduleName.toString());
+    private setTextForSwiftFile(pathToWriteFile: String, url: any, moduleName: String) {
+        const AxiosInstance = axios.create(); 
 
-        fs.writeFile(pathToWriteFile.toString(), newData, (err) => {
-            if (err) throw err;
-        });
+        AxiosInstance.get(url)
+        .then( 
+            response => {
+            const html = response.data; 
+            const data_ = String(html).toString();
+
+            let newData = data_.replace(/%_/gi, moduleName.toString());
+
+            fs.writeFile(pathToWriteFile.toString(), newData, (err) => {
+                if (err) throw err;
+            });
+            }
+        )
+        .catch(console.error); 
     }
 }
